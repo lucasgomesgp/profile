eafafdd9e495b27e6c083549c4894cc8024c8035
Lucas Gomes <lucasgomesbt@gmail.com>
2022-05-27 17:42:14 -0300

    feat: add animations to link page

--- src/pages/links/index.tsx
+++ src/pages/links/index.tsx
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
 import { Header } from "../../components/Header";
 import GlobalStyle from "../../../styles/global";
-import { Container } from "./styles";
+import { Container, Icons, ImgArea, Text, Wrapper } from "./styles";
 
 export default function Links() {
     return (
@@ -22,6 +23,28 @@ export default function Links() {
                         Setup
                     </Link>
                 </Header>
+                <Wrapper>
+                    <ImgArea>
+                        <Image src="/assets/avatar.png" width={100} height={100} layout="fixed" quality={100} alt="Lucas Gomes" />
+                        <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg" id="mouse">
+                            <path d="M21.8369 1.16506L2.34719 10.1604C0.0985938 11.2099 0.848125 14.508 3.24672 14.508H11.4925V22.7538C11.4925 25.1524 14.7906 25.9024 15.8402 23.6533L24.8355 4.16365C25.585 2.36412 23.6359 0.415056 21.8369 1.16506V1.16506Z" fill="url(#paint0_linear_84_2440)" stroke="white" />
+                            <defs>
+                                <linearGradient id="paint0_linear_84_2440" x1="13.0003" y1="1.00012" x2="13.0003" y2="25.0004" gradientUnits="userSpaceOnUse">
+                                    <stop offset="0.00520833" stopColor="#BC7ADD" />
+                                    <stop offset="0.328125" stopColor="#5C59DF" />
+                                    <stop offset="0.494792" stopColor="#6F48DF" />
+                                    <stop offset="0.666667" stopColor="#8A20DD" />
+                                    <stop offset="0.927083" stopColor="#B00FFF" />
+                                </linearGradient>
+                            </defs>
+                        </svg>
+                    </ImgArea>
+                    <Text>
+                        Follow Me
+                    </Text>
+                    <Icons>
+                    </Icons>
+                </Wrapper>
             </Container>
         </>
     );
--- src/pages/links/styles.ts
+++ src/pages/links/styles.ts
@@ -1,5 +1,58 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const mouseClick = keyframes`
+  from{
+    transform: translateX(0) translateY(0);
+    }
+    50%{
+      transform: translateX(100px);
+    }
+  to{
+    transform:  translateY(100px);
+  /* top: 0; */
+  /* width: 22px;
+  height: 22px; */
+  }
+`;
 
 const Container = styled.main``;
 
-export { Container };
+const Wrapper = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  color: #ffffff;
+  gap: 1rem;
+`;
+
+const ImgArea = styled.div`
+  display: flex;
+  align-items: center;
+  background: linear-gradient(
+    180deg,
+    #bc7add 0.52%,
+    #5c59df 32.81%,
+    #6f48df 49.48%,
+    #8a20dd 66.67%,
+    #b00fff 92.71%
+  );
+  border-radius: 50%;
+  padding: 0.25rem;
+  position: relative;
+
+  img {
+  }
+
+  #mouse {
+    position: absolute;
+    animation: ${mouseClick} 1s alternate linear forwards infinite;
+    top: -20px;
+  }
+`;
+
+const Text = styled.h3``;
+
+const Icons = styled.div``;
+
+export { Container, Wrapper, ImgArea, Text, Icons };
